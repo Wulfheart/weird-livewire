73454c2c5367dfa3b17d74b32f127612e25e77c6
Wulfheart <hi@alexfwulf.de>
2020-07-03 23:21:23 +0200

    Vue

--- resources/js/components/VizComponent.vue
+++ resources/js/components/VizComponent.vue
@@ -1,23 +1,53 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
+    <div>
+        <div>
+            <label>Midi File</label>
+            <input type="file" name="" accept="audio/midi" v-on:input="input" :value="midi.value">
         </div>
+        <div>
+            <input type="color" name="" id="">
+        </div>
+        <button type="button" v-on:click="submit">Submit</button>
     </div>
 </template>
 
 <script>
+    import "axios"
+    import Axios from 'axios';
     export default {
-        mounted() {
-            console.log('Component mounted.')
+        data: function () {
+            return {
+                midi: {
+                    value: ""
+                },
+
+            }
+        },
+        methods: {
+            input: function (event) {
+                this.midi.value = event.target.value;
+                console.log(event.target.value)
+                this.$emit('input', event.target.value)
+            },
+            submit: function () {
+                bodyFormData = new FormData();
+                bodyFormData.append('midi', midi.value)
+                Axios.post('./visualize',
+                        bodyFormData,
+                        {
+                            'Content-Type': 'multipart/form-data'
+                        })
+                    .then(function (response) {
+                        //handle success
+                        console.log(response);
+                    })
+                    .catch(function (response) {
+                        //handle error
+                        console.log(response);
+                    });
+
+            }
         }
     }
+
 </script>
