9f6a38f1613b20f339103674b34a8865bcc5a2a1
Wulfheart <hi@alexfwulf.de>
2020-07-03 22:01:01 +0200

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('inputform');
+});
+
+Route::post('visualize', function(Request $request){
+    dd($request->all());
+})->name('viz.post');
